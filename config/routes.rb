4ed360b89a07f45a68b64be571c2db86782037a8
suzu784 <s.suzuki784@hotmail.com>
2022-06-09 09:17:27 +0000

    20回目のこみっとです

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about", as: "about"
   
-  #機能を追加します
-  
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
